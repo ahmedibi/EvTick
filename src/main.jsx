9604e6f4842df2a7911e26ab8fd0acba854eb02d
Ahmed Ibrahim <ahmedibrahim2k25@gmail.com>
2025-11-30 15:21:36 +0200

    fix: replace project structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./tailwind.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import './tailwind.css'
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <h1 className="text-3xl bg-red-300 font-bold underline">
-      Hello, Tailwind CSS!
-    </h1>
-  </StrictMode>
-);
+    <h1 className="text-3xl bg-red-300 font-bold underline">Hello, Tailwind CSS!</h1>
+  </StrictMode>,
+)
