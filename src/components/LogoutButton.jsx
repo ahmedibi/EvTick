135f2a59e3dbf4c855fbf940872571e06dc005df
marihan <marihantaha355@gmail.com>
2025-12-09 17:18:10 +0200

    modified:   src/components/LogoutButton.jsx 	modified:   src/features/profile/ProfileLayout.jsx 	modified:   src/index.css

--- src/components/LogoutButton.jsx
+++ src/components/LogoutButton.jsx
@@ -1,22 +1,26 @@
 //import { useAuth } from "../context/AuthContext";
 import { logoutUser } from "../features/auth/authSlice";
 import { signOut } from "firebase/auth";
-import { auth } from "../features/firebase/config";
+import { auth } from "../firebase/firebase.config";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 export default function LogoutButton() {
   
 const dispatch = useDispatch();
+const navigate = useNavigate();
+
 
 const logout = async () => {
   await signOut(auth);
   dispatch(logoutUser());
+  navigate("/");
 };
 
   return (
     <button
       onClick={logout}
-      className="px-4 py-2 bg-red-500 text-white rounded"
+      className="px-4 py-2 bg-black text-white rounded"
     >
       Logout
     </button>
